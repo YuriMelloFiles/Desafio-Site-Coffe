23cd91c37ba8cc35a5a6fc20e215c2080e6a00f6
Yuri Mello <58647179+YuriMelloFiles@users.noreply.github.com>
2022-05-08 17:11:22 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-/* abre e fecha o menu ao clicar no icone */
-
+/* menu toggle */
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
@@ -9,49 +8,7 @@ for (const element of toggle) {
   })
 }
 
-/* quando clicar em um item do menu, esconder o menu */
-
-const links = document.querySelectorAll('nav ul li a')
-
-for (const link of links) {
-  link.addEventListener('click', function () {
-    nav.classList.remove('show')
-  })
-}
-
-/* mudar o header da página quando der scroll */
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
-
-function changeHeaderWhenScroll() {
-  if (window.scrollY >= navHeight) {
-    /* scroll > header */
-    header.classList.add('scroll')
-  } else {
-    /* scroll < header*/
-    header.classList.remove('scroll')
-  }
-}
-
-/* testimonials swiper */
-
-const swiper = new Swiper('.swiper', {
-  slidesPerView: 1,
-  pagination: {
-    el: '.swiper-pagination'
-  },
-  mousewheel: true,
-  keyboard: true,
-  breakpoints: {
-    767: {
-      slidesPerView: 2,
-      setWrapper: true
-    }
-  }
-})
-
 /* mostrando elementos quando der scroll na página */
-
 const scrollReveal = ScrollReveal({
   origin: 'top',
   distance: '30px',
@@ -59,54 +16,6 @@ const scrollReveal = ScrollReveal({
   reset: true
 })
 
-scrollReveal.reveal(
-  `#home .image, #home .text , #about .image, #about .text, #services header, #services .card, #testimonials header, #testimonials .testimonials,#contact .text, #contact .links,footer .brand,footer .social `,
-  {
-    interval: 100
-  }
-)
-
-/* Botão de rolar para o topo */
-const backToTopButton = document.querySelector('.back-to-top')
-
-function backToTop() {
-  if (window.scrollY >= 560) {
-    backToTopButton.classList.add('show')
-  } else {
-    backToTopButton.classList.remove('show')
-  }
-}
-
-/* Menu ativo conforme a seção visível na página */
-const sections = document.querySelectorAll('main section[id]')
-
-function activateMenuAtCurrentSection() {
-  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
-
-  for (const section of sections) {
-    const sectionTop = section.offsetTop
-    const sectionHeight = section.offsetHeight
-    const sectionId = section.getAttribute('id')
-
-    const checkpointStart = checkpoint >= sectionTop
-    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
-
-    if (checkpointStart && checkpointEnd) {
-      document
-        .querySelector('nav ul li a[href*= ' + sectionId + ']')
-        .classList.add('active')
-    } else {
-      document
-        .querySelector('nav ul li a[href*=' + sectionId + ']')
-        .classList.remove('active')
-    }
-  }
-}
-
-/* when scroll*/
-
-window.addEventListener('scroll', function () {
-  changeHeaderWhenScroll()
-  backToTop()
-  activateMenuAtCurrentSection()
+scrollReveal.reveal(`main .content, main .text, main .stroke, main .image`, {
+  interval: 100
 })
